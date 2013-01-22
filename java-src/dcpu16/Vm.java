e3c7696b3f3e351b50a0590a6758c55616d6cc37
Corey <corey@undiwork>
2013-01-22 13:18:23 -0500

    Work on java VM, need to rething get/set val, possibly move into separate class due to getNextWord getting called too many times with the naive implementation I currently have. Possibly store last two getNextWords? reset on op?

--- java-src/dcpu16/Vm.java
+++ java-src/dcpu16/Vm.java
@@ -2,6 +2,9 @@ package dcpu16;
 
 public class Vm{
 		private int cycles = 0;
+		private int pc = 0;
+		private int ex = 0;
+		private int sp = 0xffff;
 		private int[] registers = new int[8];
 		private int[] ram = new int[0x1000];
 
@@ -32,5 +35,117 @@ public class Vm{
 		}
 		public int getCycles(){
 				return cycles;
+		}
+		public int getNextWord(){
+				return getShort(ramGet(++pc));
+		}
+		/**
+		 * Gets the value requested from the code word
+		 * -- May increase PC while getting next word --
+		 * @param
+		 *   val - the 5/6 bit value indicator
+		 **/
+		private int getVal(int val){
+				//Register
+				if(val <= 0x07){
+						return regGet(val);
+				}
+				//Register mem
+				if(val <= 0x0f){
+						return ramGet(regGet(val - 0x08));
+				}
+				//Register mem + offset
+				if(val <= 0x17){
+						return ramGet(regGet(val - 0x10) + getNextWord());
+				}
+				//Pop
+				if(val == 0x18){
+						return ramGet(sp++);
+				}
+				//Peek
+				if(val == 0x19){
+						return ramGet(sp);
+				}
+				//Pick n
+				if(val == 0x1a){
+						return ramGet(sp + getNextWord());
+				}
+				//SP
+				if(val == 0x1b){
+						return sp;
+				}
+				//PC
+				if(val == 0x1c){
+						return pc;
+				}
+				//EX
+				if(val == 0x1d){
+						return ex;
+				}
+				//Mem
+				if(val == 0x1e){
+						return ramGet(getNextWord());
+				}
+				//Literal next word
+				if(val == 0x1f){
+						return getNextWord();
+				}
+				//Literal
+				return val - 0x20;
+		}
+		/**
+		 * sets a code value (b-value)
+		 * @params
+		 *  val - the 5-bit value to store to
+		 *  newValue - the new value to set
+		 **/
+		private void setVal(int val, int newValue){
+				//Register
+				if(val <= 0x07){
+						regSet(val, newValue);
+				}
+				//Register mem
+				if(val <= 0x0f){
+						ramSet(regGet(val - 0x08), newValue);
+				}
+				//Register mem + offset
+				if(val <= 0x17){
+						//TODO: this will cause the getNextWord to be called too many times...
+						ramSet(regGet(val - 0x08) + getNextWord(), newValue);
+				}
+				//PUSH
+				if(val == 0x18){
+						ramSet(--sp, newValue);
+				}
+				//PEEK
+				if(val == 0x19){
+						ramSet(sp, newValue);
+				}
+				//PICK
+				if(val == 0x1a){
+						//TODO: this will cause the getNextWord to be called too many times...
+						ramSet(sp + getNextWord(), newValue);
+				}
+				//SP
+				if(val == 0x1b){
+						sp = newValue;
+				}
+				//PC
+				if(val == 0x1c){
+						pc = newValue;
+				}
+				//EX
+				if(val == 0x1d){
+						ex = newValue;
+				}
+				//mem
+				if(val == 0x1e){
+						//TODO: this will cause the getNextWord to be called too many times...
+						ramSet(getNextWord(), newValue);
+				}
+				//next word lit
+				if(val == 0x1f){
+						//fail silently
+				}
 		}
 }
